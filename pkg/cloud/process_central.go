ad766357955c00ef9fee30707915e59ac8b44bcf
acosta <alexmcosta.dev@gmail.com>
2019-06-04 07:58:00 -0700

    Added comments to ProcessCentral()

--- pkg/cloud/process_central.go
+++ pkg/cloud/process_central.go
@@ -1,5 +1,8 @@
 package cloud
 
+// Process Central uses channels and go routines to run every resources processes concurrently
+// It is the great Orchestrator of the cloud package
 func ProcessCentral(tags *Tags) string {
+
 	return "Process Central Complete"
 }
